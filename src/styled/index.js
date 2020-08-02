3d85cad2a3d8de461b97084d0fa50015b8f5e63a
Pau Bonilla <paulo111995@gmail.com>
2020-08-01 23:59:15 -0700

    first commit

---
+++ src/styled/index.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const AppWrapper = styled.div`
+  margin: 0 auto;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const AppInner = styled.div`
+  width: 100%;
+  max-width: 800px;
+`
+
+export const BoardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const GameStatusContainer = styled.div`
+  display: flex;
+`
